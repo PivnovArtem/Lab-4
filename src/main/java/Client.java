03f606939fed110fc29ac10f0f1b2033a42c220b
PivnovArtem <35925144+PivnovArtem@users.noreply.github.com>
2019-04-02 00:53:52 +0300

    feat: app

---
+++ src/main/java/Client.java
@@ -0,0 +1,67 @@
+import io.vertx.core.*;
+import io.vertx.core.eventbus.Message;
+
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class Client extends AbstractVerticle {
+    @Override
+    public void start()  {
+        try {
+            vertx.eventBus().consumer("address").handler((e) -> {
+                vertx.executeBlocking(future -> {
+                    System.out.println("its ok? " + e.body());
+                    String result = input();
+
+                    future.complete(result);
+                }, res -> {
+                    if (res.result() != "") {
+                        e.reply(e.body() + " " + res.result());
+                        System.out.println(e.body() + " " + res.result());
+                    }
+                    else {
+                        System.out.println(e.body() + " не обработано");
+                    }
+                });
+            });
+        } catch (VertxException e) {
+            System.out.println("Error");
+            throw new VertxException(e);
+        }
+    }
+
+    private String input() {
+        FutureTask<String> readNextLine = new FutureTask<String>(() -> {
+            String scanner = new Scanner(System.in).nextLine();
+            return scanner;
+        });
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.execute(readNextLine);
+        String token = "";
+        try {
+            token = readNextLine.get(50000L, TimeUnit.MILLISECONDS);
+            executor.shutdown();
+        } catch (TimeoutException e) {
+            readNextLine.cancel(true);
+            executor.shutdownNow();
+            executor.shutdown();
+            System.out.println("Выполнение потока прервано" + System.currentTimeMillis());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println("Выполнение потока прервано" + System.currentTimeMillis());
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            System.out.println("Выполнение потока прервано 2" + System.currentTimeMillis());
+        }
+        executor.shutdown();
+        return token;
+    }
+
+    public static void main(String[] args){
+        Vertx.clusteredVertx(new VertxOptions(), (event ->
+                event.result().deployVerticle(new NotMainVerticle(), new DeploymentOptions().setWorker(false))));
+    }
+}
+
+
---
+++ src/main/java/Server.java
@@ -0,0 +1,45 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.eventbus.DeliveryOptions;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+
+import java.util.Scanner;
+
+public class Server extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+        HttpServer server = vertx.createHttpServer();
+
+        server.requestHandler(request -> {
+
+            HttpServerResponse response = request.response();
+            response.putHeader("content-type", "text/plain");
+
+            response.end("Hello World!");
+            printResult(request.params().get("param"));
+        });
+        server.listen(8000);
+    }
+
+    public void printResult(String comment) {
+        DeliveryOptions optionsD = new DeliveryOptions();
+        optionsD.setSendTimeout(51000);
+        vertx.eventBus().send("address", comment, optionsD,  (r) ->{
+            if (r.succeeded()){
+                System.out.println(r.result().body().toString());
+            } else {
+                System.out.println("redirect " + comment);
+                printResult(comment);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Vertx.clusteredVertx(new VertxOptions(), (event ->
+                event.result().deployVerticle(new MainVerticle())));
+    }
+}
+
